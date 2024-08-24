9319016c6197d9588b885fcfd671117310f11078
anatoliyKaragodin <anatoliy.karagodin@gmail.com>
2024-08-24 22:36:31 +0400

    Правка грамматической ошибки

---
+++ test/features/file_managment/data/datasources/local/file_lds_test.dart
@@ -0,0 +1,33 @@
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:fate_app/features/file_managment/data/datasources/local/file_LDS_impl.dart';
+// import 'package:file_picker/file_picker.dart';
+// import 'dart:io';
+// import 'package:path_provider/path_provider.dart';
+
+// void main() {
+//     TestWidgetsFlutterBinding.ensureInitialized(); // Инициализация привязки
+
+//   group('FileLdsImpl', () {
+//     late FileLdsImpl fileLdsImpl;
+//     // late Directory tempDir;
+
+//     setUp(() async {
+//       fileLdsImpl = FileLdsImpl();
+//       // tempDir = await getTemporaryDirectory();
+//     });
+
+//     test('should save and delete file successfully', () async {
+//       final fileName = 'test_file.txt';
+//       // final filePath = '${tempDir.path}/$fileName';
+//       final mockFile = PlatformFile(name: fileName, size: 10);
+
+//       // Сохранение файла
+//       final filePath=await fileLdsImpl.save(mockFile);
+//       expect(await File(filePath!).exists(), isTrue);
+
+//       // Удаление файла
+//       await fileLdsImpl.delete(filePath);
+//       expect(await File(filePath).exists(), isFalse);
+//     });
+//   });
+// }
